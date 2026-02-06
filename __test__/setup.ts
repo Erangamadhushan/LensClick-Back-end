e3fab6d91b7ddabdd03de4bd62b381af24ce2958
Erangamadhushan <erangamadhushann956@gmail.com>
2026-02-07 01:12:50 +0530

    chore: update dotenv configuration to use .env.test.v1

--- __test__/setup.ts
+++ __test__/setup.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 
 dotenv.config({
-  path: ".env.test",
+  //path: ".env.test",
+  path: ".env.test.v1",
   override: true,
   debug: false,
 });
